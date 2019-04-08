f72edf727959bda640e63df60a492a0ece3a400d
Juhana Jauhiainen <juhana.jauhiainen@gmail.com>
2019-04-08 23:06:33 +0300

    Project renamed. Added gitignore.

--- DjangoUserRegistrationExample/wsgi.py
+++ DjangoUserRegistrationExample/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for DjangoUserRegistrationTemplate project.
+WSGI config for DjangoUserRegistrationExample project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'DjangoUserRegistrationTemplate.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'DjangoUserRegistrationExample.settings')
 
 application = get_wsgi_application()
